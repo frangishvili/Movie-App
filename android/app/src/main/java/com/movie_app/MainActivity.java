fa3376f315c1309ff8e45e630bdb9cc7c3db5ff8
Avtandil Prangishvili <avtofrangi@gmail.com>
2023-12-30 21:14:10 +0400

    chore: Add react navigaion package

--- android/app/src/main/java/com/movie_app/MainActivity.java
+++ android/app/src/main/java/com/movie_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.movie_app;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -11,6 +12,10 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  @override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+
   @Override
   protected String getMainComponentName() {
     return "Movie_App";
