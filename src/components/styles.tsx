2c2af6ddc980e261b43c7709b1ec0725fe2158b8
Avtandil Prangishvili <avtofrangi@gmail.com>
2023-12-31 19:50:00 +0400

    chore: Add styles file for components

---
+++ src/components/styles.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native'
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme'
+
+const styles = StyleSheet.create({
+  inputBox: {
+    display: 'flex',
+    paddingVertical: SPACING.space_8,
+    paddingHorizontal: SPACING.space_24,
+    borderWidth: 2,
+    borderColor: COLORS.WhiteRGBA15,
+    borderRadius: BORDERRADIUS.radius_25,
+    flexDirection: 'row',
+  },
+  textInput: {
+    width: '90%',
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+  },
+  searchIcon: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: SPACING.space_10,
+  },
+})
+
+export default styles
