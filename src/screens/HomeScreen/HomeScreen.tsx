ce77a38eda43e976d2c4c1a74a9b63e71bd077de
Avtandil Prangishvili <avtofrangi@gmail.com>
2024-01-02 00:36:48 +0400

    chore: Update Home screen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState } from 'react'
+import { View, Text, Dimensions, ActivityIndicator, StatusBar, ScrollView } from 'react-native'
+import { upcomingMovies, nowPlayingMovies, popularMovies, baseImagePath } from '../../api/apicall'
 
-import styles from './styles';
+import InputHeader from '../../components/InputHeader'
+
+import { COLORS } from '../../theme/theme'
+
+const { width, height } = Dimensions.get('window')
+
+import styles from './styles'
 
 const HomeScreen = () => {
+  const [nowPlayingMoviesList, setNowPlayingMoviesList] = useState<any>(undefined)
+  const [popularMoviesList, setPopularMoviesList] = useState<any>(undefined)
+  const [upcomingMoviesList, setUpcomingMoviesList] = useState<any>(undefined)
+
+  if (
+    nowPlayingMoviesList == undefined &&
+    nowPlayingMoviesList == null &&
+    popularMoviesList == undefined &&
+    popularMoviesList == null &&
+    upcomingMoviesList == undefined &&
+    upcomingMoviesList == null
+  ) {
+    return (
+      <ScrollView style={styles.container} bounces={false} contentContainerStyle={styles.scrollViewContainer}>
+        <StatusBar hidden />
+
+        <View style={styles.InputHeaderContainer}>
+          <InputHeader />
+        </View>
+
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size={'large'} color={COLORS.Orange} />
+        </View>
+      </ScrollView>
+    )
+  }
   return (
     <View>
       <Text>HomeScreen</Text>
     </View>
-  );
-};
+  )
+}
 
-export default HomeScreen;
+export default HomeScreen
