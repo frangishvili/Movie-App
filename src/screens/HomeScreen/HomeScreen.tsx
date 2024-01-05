44cc72c690a8c918a4b03f928d4912cceda6a56a
Avtandil Prangishvili <avtofrangi@gmail.com>
2024-01-05 13:49:13 +0400

    chore: Update HomeScreen component

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,19 +1,69 @@
-import React, { useState } from 'react'
-import { View, Text, Dimensions, ActivityIndicator, StatusBar, ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, FlatList, Dimensions, ActivityIndicator, StatusBar, ScrollView } from 'react-native'
 import { upcomingMovies, nowPlayingMovies, popularMovies, baseImagePath } from '../../api/apicall'
 
 import InputHeader from '../../components/InputHeader'
+import CategoryHeader from '../../components/CategoryHeader'
+import SubMovieCard from '../../components/SubMoviesCard'
+import MovieCard from '../../components/MovieCard'
 
-import { COLORS } from '../../theme/theme'
+import { COLORS, SPACING } from '../../theme/theme'
 
 const { width, height } = Dimensions.get('window')
 
 import styles from './styles'
 
-const HomeScreen = () => {
+const getNowPlayingMoviesList = async () => {
+  try {
+    let response = await fetch(nowPlayingMovies)
+    let json = await response.json()
+    return json
+  } catch (error) {
+    console.error(' Something went wrong in getNowPlayingMoviesList Function', error)
+  }
+}
+
+const getUpcomingMoviesList = async () => {
+  try {
+    let response = await fetch(upcomingMovies)
+    let json = await response.json()
+    return json
+  } catch (error) {
+    console.error(' Something went wrong in getUpcomingMoviesList Function', error)
+  }
+}
+
+const getPopularMoviesList = async () => {
+  try {
+    let response = await fetch(popularMovies)
+    let json = await response.json()
+    return json
+  } catch (error) {
+    console.error(' Something went wrong in getPopularMoviesList Function', error)
+  }
+}
+
+const HomeScreen = ({ navigation }: any) => {
   const [nowPlayingMoviesList, setNowPlayingMoviesList] = useState<any>(undefined)
   const [popularMoviesList, setPopularMoviesList] = useState<any>(undefined)
   const [upcomingMoviesList, setUpcomingMoviesList] = useState<any>(undefined)
+
+  useEffect(() => {
+    ;(async () => {
+      let tempNowPlaying = await getNowPlayingMoviesList()
+      setNowPlayingMoviesList([{ id: 'dummy1' }, ...tempNowPlaying.results, { id: 'dummy2' }])
+
+      let tempPopular = await getPopularMoviesList()
+      setPopularMoviesList(tempPopular.results)
+
+      let tempUpcoming = await getUpcomingMoviesList()
+      setUpcomingMoviesList(tempUpcoming.results)
+    })()
+  }, [])
+
+  const searchMoviesFunction = () => {
+    navigation.navigate('Search')
+  }
 
   if (
     nowPlayingMoviesList == undefined &&
@@ -38,9 +88,94 @@ const HomeScreen = () => {
     )
   }
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <ScrollView style={styles.container} bounces={false}>
+      <StatusBar hidden />
+      <View style={styles.InputHeaderContainer}>
+        <InputHeader searchFunction={searchMoviesFunction} />
+      </View>
+      <CategoryHeader title={'Now Playing'} />
+      <FlatList
+        data={nowPlayingMoviesList}
+        keyExtractor={(item: any) => item.id}
+        bounces={false}
+        snapToInterval={width * 0.7 + SPACING.space_36}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        decelerationRate={0}
+        contentContainerStyle={styles.containerGap36}
+        renderItem={({ item, index }) => {
+          if (!item.original_title) {
+            return (
+              <View
+                style={{
+                  width: (width - (width * 0.7 + SPACING.space_36 * 2)) / 2,
+                }}
+              ></View>
+            )
+          }
+          return (
+            <MovieCard
+              shoudlMarginatedAtEnd={true}
+              cardFunction={() => {
+                navigation.push('MovieDetails', { movieid: item.id })
+              }}
+              cardWidth={width * 0.7}
+              isFirst={index == 0 ? true : false}
+              isLast={index == upcomingMoviesList?.length - 1 ? true : false}
+              title={item.original_title}
+              imagePath={baseImagePath('w780', item.poster_path)}
+              genre={item.genre_ids.slice(1, 4)}
+              vote_average={item.vote_average}
+              vote_count={item.vote_count}
+            />
+          )
+        }}
+      />
+      <CategoryHeader title={'Popular'} />
+      <FlatList
+        data={popularMoviesList}
+        keyExtractor={(item: any) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+        contentContainerStyle={styles.containerGap36}
+        renderItem={({ item, index }) => (
+          <SubMovieCard
+            shoudlMarginatedAtEnd={true}
+            cardFunction={() => {
+              navigation.push('MovieDetails', { movieid: item.id })
+            }}
+            cardWidth={width / 3}
+            isFirst={index == 0 ? true : false}
+            isLast={index == upcomingMoviesList?.length - 1 ? true : false}
+            title={item.original_title}
+            imagePath={baseImagePath('w342', item.poster_path)}
+          />
+        )}
+      />
+      <CategoryHeader title={'Upcoming'} />
+      <FlatList
+        data={upcomingMoviesList}
+        keyExtractor={(item: any) => item.id}
+        horizontal
+        bounces={false}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.containerGap36}
+        renderItem={({ item, index }) => (
+          <SubMovieCard
+            shoudlMarginatedAtEnd={true}
+            cardFunction={() => {
+              navigation.push('MovieDetails', { movieid: item.id })
+            }}
+            cardWidth={width / 3}
+            isFirst={index == 0 ? true : false}
+            isLast={index == upcomingMoviesList?.length - 1 ? true : false}
+            title={item.original_title}
+            imagePath={baseImagePath('w342', item.poster_path)}
+          />
+        )}
+      />
+    </ScrollView>
   )
 }
 
