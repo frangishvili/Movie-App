7e29b77e1b294e515d3b3da753ac77475518f489
Avtandil Prangishvili <avtofrangi@gmail.com>
2024-01-05 13:50:30 +0400

    chore: Add styles for Home screen

---
+++ src/screens/HomeScreen/styles.tsx
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+import {COLORS, SPACING} from '../../theme/theme';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    backgroundColor: COLORS.Black,
+  },
+  scrollViewContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  InputHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_28,
+  },
+  containerGap36: {
+    gap: SPACING.space_36,
+  },
+});
+
+export default styles;
