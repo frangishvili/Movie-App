8eaddab3cf630b3b47edad333bba4705e1e4037b
Avtandil Prangishvili <avtofrangi@gmail.com>
2024-01-05 13:51:18 +0400

    chore: Update Search screen

--- src/screens/SearchScreen/SearchScreen.tsx
+++ src/screens/SearchScreen/SearchScreen.tsx
@@ -1,14 +1,59 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, { useState } from 'react'
+import { View, Dimensions, StatusBar, FlatList } from 'react-native'
+import { SPACING } from '../../theme/theme'
+import { baseImagePath, searchMovies } from '../../api/apicall'
+import InputHeader from '../../components/InputHeader'
+import SubMovieCard from '../../components/SubMoviesCard'
 
-import styles from './styles';
+const { width, height } = Dimensions.get('screen')
 
-const SearchScreen = () => {
+import styles from './styles'
+
+const SearchScreen = ({ navigation }: any) => {
+  const [searchList, setSearchList] = useState([])
+
+  const searchMoviesFunction = async (name: string) => {
+    try {
+      let response = await fetch(searchMovies(name))
+      let json = await response.json()
+      setSearchList(json.results)
+    } catch (error) {
+      console.error('Something went wrong in searchMoviesFunction ', error)
+    }
+  }
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <View>
+        <FlatList
+          data={searchList}
+          keyExtractor={(item: any) => item.id}
+          bounces={false}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={
+            <View style={styles.InputHeaderContainer}>
+              <InputHeader searchFunction={searchMoviesFunction} />
+            </View>
+          }
+          contentContainerStyle={styles.centerContainer}
+          renderItem={({ item, index }) => (
+            <SubMovieCard
+              shoudlMarginatedAtEnd={false}
+              shouldMarginatedAround={true}
+              cardFunction={() => {
+                navigation.push('MovieDetails', { movieid: item.id })
+              }}
+              cardWidth={width / 2 - SPACING.space_12 * 2}
+              title={item.original_title}
+              imagePath={baseImagePath('w342', item.poster_path)}
+            />
+          )}
+        />
+      </View>
     </View>
-  );
-};
+  )
+}
 
-export default SearchScreen;
+export default SearchScreen
---
+++ src/screens/SearchScreen/styles.tsx
@@ -0,0 +1,24 @@
+import {StyleSheet, Dimensions} from 'react-native';
+import {COLORS, SPACING} from '../../theme/theme';
+const {width} = Dimensions.get('screen');
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    width,
+    alignItems: 'center',
+    backgroundColor: COLORS.Black,
+  },
+  InputHeaderContainer: {
+    display: 'flex',
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_28,
+    marginBottom: SPACING.space_28 - SPACING.space_12,
+  },
+  centerContainer: {
+    alignItems: 'center',
+  },
+});
+
+export default styles;
