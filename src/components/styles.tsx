d0a0a6a7b9240a86498b9a4a6a0c367e770120ae
Avtandil Prangishvili <avtofrangi@gmail.com>
2024-01-16 19:51:58 +0400

    chore: add styles

--- src/components/styles.tsx
+++ src/components/styles.tsx
@@ -22,6 +22,152 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: SPACING.space_10,
   },
+  text: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_20,
+    color: COLORS.White,
+    paddingHorizontal: SPACING.space_36,
+    paddingVertical: SPACING.space_28,
+  },
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: COLORS.Black,
+  },
+  cardImage: {
+    aspectRatio: 2 / 3,
+    borderRadius: BORDERRADIUS.radius_20,
+  },
+  textTitle: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+    textAlign: 'center',
+    paddingVertical: SPACING.space_10,
+  },
+  movieCardContainer: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: COLORS.Black,
+  },
+  MovieCardImage: {
+    aspectRatio: 2 / 3,
+    borderRadius: BORDERRADIUS.radius_20,
+  },
+  movieTextTitle: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.White,
+    textAlign: 'center',
+    paddingVertical: SPACING.space_10,
+  },
+  rateContainer: {
+    flexDirection: 'row',
+    gap: SPACING.space_10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: SPACING.space_10,
+  },
+  starIcon: {
+    fontSize: FONTSIZE.size_20,
+    color: COLORS.Yellow,
+  },
+  voteText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+  },
+  genreContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: SPACING.space_20,
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  genreBox: {
+    borderColor: COLORS.WhiteRGBA50,
+    borderWidth: 1,
+    paddingVertical: SPACING.space_4,
+    paddingHorizontal: SPACING.space_10,
+    borderRadius: BORDERRADIUS.radius_25,
+  },
+  genreText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.WhiteRGBA75,
+  },
+  appContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iconStyle: {
+    color: COLORS.White,
+    fontSize: FONTSIZE.size_24,
+  },
+  headerText: {
+    flex: 1,
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_20,
+    textAlign: 'center',
+    color: COLORS.White,
+  },
+  emptyContainer: {
+    height: SPACING.space_20 * 2,
+    width: SPACING.space_20 * 2,
+  },
+  iconBG: {
+    height: SPACING.space_20 * 2,
+    width: SPACING.space_20 * 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: BORDERRADIUS.radius_20,
+    backgroundColor: COLORS.Orange,
+  },
+  castCardContainer: {
+    alignItems: 'center',
+  },
+  castCardImage: {
+    aspectRatio: 1920 / 2880,
+    borderRadius: BORDERRADIUS.radius_25 * 4,
+  },
+  title: {
+    alignSelf: 'stretch',
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.White,
+  },
+  subtitle: {
+    alignSelf: 'stretch',
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.White,
+  },
+  settingWrpper: {
+    flexDirection: 'row',
+    paddingVertical: SPACING.space_20,
+  },
+  settingContainer: {
+    flex: 1,
+  },
+  settingIconStyle: {
+    color: COLORS.White,
+    fontSize: FONTSIZE.size_24,
+    paddingHorizontal: SPACING.space_20,
+  },
+  settingIconBG: {
+    justifyContent: 'center',
+  },
+  settingTitle: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.White,
+  },
+  settingSubtitle: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.WhiteRGBA15,
+  },
 })
 
 export default styles
