4fa3a23085f89673a16471ab3935a05953b24195
Avtandil Prangishvili <avtofrangi@gmail.com>
2024-01-20 19:11:49 +0400

    chore: Add MovieDetails Screen

--- src/screens/MovieDetailsScreen/MovieDetailsScreen.tsx
+++ src/screens/MovieDetailsScreen/MovieDetailsScreen.tsx
@@ -1,14 +1,173 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react'
+import {
+  Text,
+  View,
+  ScrollView,
+  ActivityIndicator,
+  StatusBar,
+  ImageBackground,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native'
+import { baseImagePath, movieCastDetails, movieDetails } from '../../api/apicall'
+import { COLORS } from '../../theme/theme'
+import AppHeader from '../../components/AppHeader'
+import LinearGradient from 'react-native-linear-gradient'
+import CustomIcon from '../../components/CustomIcon'
+import CategoryHeader from '../../components/CategoryHeader'
+import CastCard from '../../components/CastCard'
 
-import styles from './styles';
+import styles from './styles'
 
-const MovieDetailsScreen = () => {
+const getMovieDetails = async (movieid: number) => {
+  try {
+    let response = await fetch(movieDetails(movieid))
+    let json = await response.json()
+    return json
+  } catch (error) {
+    console.error('Something Went wrong in getMoviesDetails Function', error)
+  }
+}
+
+const getMovieCastDetails = async (movieid: number) => {
+  try {
+    let response = await fetch(movieCastDetails(movieid))
+    let json = await response.json()
+    return json
+  } catch (error) {
+    console.error('Something Went wrong in getMovieCastDetails Function', error)
+  }
+}
+
+const MovieDetailsScreen = ({ navigation, route }: any) => {
+  const [movieData, setMovieData] = useState<any>(undefined)
+  const [movieCastData, setmovieCastData] = useState<any>(undefined)
+
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      const movieData = await getMovieDetails(route.params.movieid)
+      const movieCastData = await getMovieCastDetails(route.params.movieid)
+      setMovieData(movieData)
+      setmovieCastData(movieCastData.cast)
+    }
+
+    fetchMovieData()
+  }, [])
+
+  if (!movieData || !movieCastData) {
+    return (
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.scrollViewContainer}
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.appHeaderContainer}>
+          <AppHeader name="close" header={''} action={() => navigation.goBack()} />
+        </View>
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size={'large'} color={COLORS.Orange} />
+        </View>
+      </ScrollView>
+    )
+  }
+
   return (
-    <View>
-      <Text>MovieDetailsScreen</Text>
-    </View>
-  );
-};
+    <ScrollView style={styles.container} bounces={false} showsVerticalScrollIndicator={false}>
+      <StatusBar hidden />
 
-export default MovieDetailsScreen;
+      <View>
+        <ImageBackground
+          source={{
+            uri: baseImagePath('w780', movieData?.backdrop_path),
+          }}
+          style={styles.imageBG}
+        >
+          <LinearGradient colors={[COLORS.BlackRGB10, COLORS.Black]} style={styles.linearGradient}>
+            <View style={styles.appHeaderContainer}>
+              <AppHeader name="close" header={''} action={() => navigation.goBack()} />
+            </View>
+          </LinearGradient>
+        </ImageBackground>
+        <View style={styles.imageBG}></View>
+        <Image source={{ uri: baseImagePath('w342', movieData?.poster_path) }} style={styles.cardImage} />
+      </View>
+
+      <View style={styles.timeContainer}>
+        <CustomIcon name="clock" style={styles.clockIcon} />
+        <Text style={styles.runtimeText}>
+          {Math.floor(movieData?.runtime / 60)}h {Math.floor(movieData?.runtime % 60)}m
+        </Text>
+      </View>
+
+      <View>
+        <Text style={styles.title}>{movieData?.original_title}</Text>
+        <View style={styles.genreContainer}>
+          {movieData?.genres.map((item: any) => {
+            return (
+              <View style={styles.genreBox} key={item.id}>
+                <Text style={styles.genreText}>{item.name}</Text>
+              </View>
+            )
+          })}
+        </View>
+        <Text style={styles.tagline}>{movieData?.tagline}</Text>
+      </View>
+
+      <View style={styles.infoContainer}>
+        <View style={styles.rateContainer}>
+          <CustomIcon name="star" style={styles.starIcon} />
+          <Text style={styles.runtimeText}>
+            {movieData?.vote_average.toFixed(1)} ({movieData?.vote_count})
+          </Text>
+          <Text style={styles.runtimeText}>
+            {movieData?.release_date.substring(8, 10)}{' '}
+            {new Date(movieData?.release_date).toLocaleString('default', {
+              month: 'long',
+            })}{' '}
+            {movieData?.release_date.substring(0, 4)}
+          </Text>
+        </View>
+        <Text style={styles.descriptionText}>{movieData?.overview}</Text>
+      </View>
+
+      <View>
+        <CategoryHeader title="Top Cast" />
+        <FlatList
+          data={movieCastData}
+          keyExtractor={(item: any) => item.id}
+          horizontal
+          contentContainerStyle={styles.containerGap24}
+          renderItem={({ item, index }) => (
+            <CastCard
+              shouldMarginatedAtEnd={true}
+              cardWidth={80}
+              isFirst={index == 0 ? true : false}
+              isLast={index == movieCastData?.length - 1 ? true : false}
+              imagePath={baseImagePath('w185', item.profile_path)}
+              title={item.original_name}
+              subtitle={item.character}
+            />
+          )}
+        />
+
+        <View>
+          <TouchableOpacity
+            style={styles.buttonBG}
+            onPress={() => {
+              navigation.push('SeatBooking', {
+                BgImage: baseImagePath('w780', movieData.backdrop_path),
+                PosterImage: baseImagePath('original', movieData.poster_path),
+              })
+            }}
+          >
+            <Text style={styles.buttonText}>Select Seats</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
+  )
+}
+
+export default MovieDetailsScreen
