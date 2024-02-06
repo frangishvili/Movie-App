e321c710c84ad0c2ac8ebd90ff75250e6321c299
Avtandil Prangishvili <avtofrangi@gmail.com>
2024-02-06 22:16:37 +0400

    chore: Add UserAccount Screen

--- src/screens/UserAccountScreen/UserAccountScreen.tsx
+++ src/screens/UserAccountScreen/UserAccountScreen.tsx
@@ -1,14 +1,31 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import * as React from 'react'
+import { Text, View, StatusBar, Image } from 'react-native'
+import AppHeader from '../../components/AppHeader'
+import SettingComponent from '../../components/SettingComponent'
 
-import styles from './styles';
+import styles from './styles'
 
-const UserAccountScreen = () => {
+const UserAccountScreen = ({ navigation }: any) => {
   return (
-    <View>
-      <Text>UserAccountScreen</Text>
-    </View>
-  );
-};
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <View style={styles.appHeaderContainer}>
+        <AppHeader name="close" header={'My Profile'} action={() => navigation.goBack()} />
+      </View>
 
-export default UserAccountScreen;
+      <View style={styles.profileContainer}>
+        <Image source={require('../assets/image/avatar.png')} style={styles.avatarImage} />
+        <Text style={styles.avatarText}>John Doe</Text>
+      </View>
+
+      <View style={styles.profileContainer}>
+        <SettingComponent icon="user" heading="Account" subheading="Edit Profile" subtitle="Change Password" />
+        <SettingComponent icon="setting" heading="Settings" subheading="Theme" subtitle="Permissions" />
+        <SettingComponent icon="dollar" heading="Offers & Refferrals" subheading="Offer" subtitle="Refferrals" />
+        <SettingComponent icon="info" heading="About" subheading="About Movies" subtitle="more" />
+      </View>
+    </View>
+  )
+}
+
+export default UserAccountScreen
