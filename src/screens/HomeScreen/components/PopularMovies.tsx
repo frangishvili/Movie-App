d9450002bfa39cd63cd2a81e9e885bc21738f6b7
Avtandil Prangishvili <avtofrangi@gmail.com>
2024-03-20 19:44:34 +0400

    chore: Add PopularMovies component

---
+++ src/screens/HomeScreen/components/PopularMovies.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { FlatList, Dimensions } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import SubMovieCard from '../../../components/SubMoviesCard'
+import CategoryHeader from '../../../components/CategoryHeader'
+import { baseImagePath } from '../../../api/apicall'
+import { Movie } from './NowPlayingMovies'
+
+import styles from '../styles'
+
+const { width } = Dimensions.get('window')
+
+interface PopularMoviesProps {
+  popularMoviesList: Movie[]
+  upcomingMoviesList: Movie[]
+}
+
+const PopularMovies = ({ popularMoviesList, upcomingMoviesList }: PopularMoviesProps) => {
+  const { push } = useNavigation()
+
+  const renderItem = ({ item, index }: { item: Movie; index: number }) => (
+    <SubMovieCard
+      shoudlMarginatedAtEnd={true}
+      cardFunction={() => {
+        push('MovieDetails', { movieid: item.id })
+      }}
+      cardWidth={width / 3}
+      isFirst={index == 0 ? true : false}
+      isLast={index == upcomingMoviesList?.length - 1 ? true : false}
+      title={item.original_title}
+      imagePath={baseImagePath('w342', item.poster_path)}
+    />
+  )
+
+  return (
+    <>
+      <CategoryHeader title={'Popular'} />
+      <FlatList
+        data={popularMoviesList}
+        keyExtractor={(item: any) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+        contentContainerStyle={styles.containerGap36}
+        renderItem={renderItem}
+      />
+    </>
+  )
+}
+
+export default PopularMovies
