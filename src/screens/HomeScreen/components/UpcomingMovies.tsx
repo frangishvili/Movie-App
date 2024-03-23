58502e3b67fb076f67fce77948f3a3626e8550f6
Avtandil Prangishvili <avtofrangi@gmail.com>
2024-03-23 20:55:49 +0400

    chore: Add UpcomingMovies component

---
+++ src/screens/HomeScreen/components/UpcomingMovies.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { FlatList, Dimensions } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import SubMovieCard from '../../../components/SubMoviesCard'
+import CategoryHeader from '../../../components/CategoryHeader'
+import { baseImagePath } from '../../../api/apicall'
+import { Movie } from './NowPlayingMovies'
+
+import styles from '../styles'
+
+const { width } = Dimensions.get('window')
+
+interface UpcomingMoviesProps {
+  upcomingMoviesList: Movie[]
+}
+
+const UpcomingMovies = ({ upcomingMoviesList }: UpcomingMoviesProps) => {
+  const { push } = useNavigation()
+
+  const renderItem = ({ item, index }: { item: Movie; index: number }) => (
+    <SubMovieCard
+      shoudlMarginatedAtEnd={true}
+      cardFunction={() => {
+        push('MovieDetails', { movieid: item.id })
+      }}
+      cardWidth={width / 3}
+      isFirst={index == 0 ? true : false}
+      isLast={index == upcomingMoviesList?.length - 1 ? true : false}
+      title={item.original_title}
+      imagePath={baseImagePath('w342', item.poster_path)}
+    />
+  )
+
+  return (
+    <>
+      <CategoryHeader title={'Upcoming'} />
+      <FlatList
+        data={upcomingMoviesList}
+        keyExtractor={(item: any) => item.id}
+        horizontal
+        bounces={false}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.containerGap36}
+        renderItem={renderItem}
+      />
+    </>
+  )
+}
+
+export default UpcomingMovies
