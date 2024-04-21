d4c7c3b9c60a5524ef19778afee93d6a5aff88a3
Avtandil Prangishvili <avtofrangi@gmail.com>
2024-04-21 23:16:16 +0200

    chore: Update movieDetails screen

--- src/screens/MovieDetailsScreen/MovieDetailsScreen.tsx
+++ src/screens/MovieDetailsScreen/MovieDetailsScreen.tsx
@@ -5,25 +5,26 @@ import {
   ScrollView,
   ActivityIndicator,
   StatusBar,
-  ImageBackground,
-  Image,
   FlatList,
   TouchableOpacity,
+  SafeAreaView,
 } from 'react-native'
-import { baseImagePath, movieCastDetails, movieDetails } from '../../api/apicall'
+
+import { baseImagePath } from '../../api/apicall'
 import { COLORS } from '../../theme/theme'
 import useMovieDetails from '../../hooks/useMovieDetails'
 import AppHeader from '../../components/AppHeader'
-import LinearGradient from 'react-native-linear-gradient'
-import CustomIcon from '../../components/CustomIcon'
 import CategoryHeader from '../../components/CategoryHeader'
 import CastCard from '../../components/CastCard'
+import MovieDetailsHeader from './components/MovieDetailsHeader'
+import MovieDetailsInfo from './components/MovieDetailsInfo'
+import MovieRate from './components/MovieRate'
 
 import styles from './styles'
 
 const MovieDetailsScreen = ({ navigation, route }: any) => {
   const [movieData, setMovieData] = useState<any>(undefined)
-  const [movieCastData, setmovieCastData] = useState<any>(undefined)
+  const [movieCastData, setMovieCastData] = useState<any>(undefined)
 
   const { getMovieDetails, getMovieCastDetails } = useMovieDetails()
 
@@ -32,7 +33,7 @@ const MovieDetailsScreen = ({ navigation, route }: any) => {
       const movieData = await getMovieDetails(route.params.movieid)
       const movieCastData = await getMovieCastDetails(route.params.movieid)
       setMovieData(movieData)
-      setmovieCastData(movieCastData.cast)
+      setMovieCastData(movieCastData.cast)
     }
 
     fetchMovieData()
@@ -40,116 +41,68 @@ const MovieDetailsScreen = ({ navigation, route }: any) => {
 
   if (!movieData || !movieCastData) {
     return (
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.scrollViewContainer}
-        bounces={false}
-        showsVerticalScrollIndicator={false}
-      >
-        <View style={styles.appHeaderContainer}>
-          <AppHeader name="close" header={''} action={() => navigation.goBack()} />
-        </View>
-        <View style={styles.loadingContainer}>
-          <ActivityIndicator size={'large'} color={COLORS.Orange} />
-        </View>
-      </ScrollView>
+      <SafeAreaView style={styles.mainContent}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.scrollViewContainer}
+          bounces={false}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={styles.appHeaderContainer}>
+            <AppHeader name="close" header={''} action={() => navigation.goBack()} />
+          </View>
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator size={'large'} color={COLORS.Orange} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     )
   }
 
   return (
-    <ScrollView style={styles.container} bounces={false} showsVerticalScrollIndicator={false}>
-      <StatusBar hidden />
-
-      <View>
-        <ImageBackground
-          source={{
-            uri: baseImagePath('w780', movieData?.backdrop_path),
-          }}
-          style={styles.imageBG}
-        >
-          <LinearGradient colors={[COLORS.BlackRGB10, COLORS.Black]} style={styles.linearGradient}>
-            <View style={styles.appHeaderContainer}>
-              <AppHeader name="close" header={''} action={() => navigation.goBack()} />
-            </View>
-          </LinearGradient>
-        </ImageBackground>
-        <View style={styles.imageBG}></View>
-        <Image source={{ uri: baseImagePath('w342', movieData?.poster_path) }} style={styles.cardImage} />
-      </View>
-
-      <View style={styles.timeContainer}>
-        <CustomIcon name="clock" style={styles.clockIcon} />
-        <Text style={styles.runtimeText}>
-          {Math.floor(movieData?.runtime / 60)}h {Math.floor(movieData?.runtime % 60)}m
-        </Text>
-      </View>
-
-      <View>
-        <Text style={styles.title}>{movieData?.original_title}</Text>
-        <View style={styles.genreContainer}>
-          {movieData?.genres.map((item: any) => {
-            return (
-              <View style={styles.genreBox} key={item.id}>
-                <Text style={styles.genreText}>{item.name}</Text>
-              </View>
-            )
-          })}
-        </View>
-        <Text style={styles.tagline}>{movieData?.tagline}</Text>
-      </View>
-
-      <View style={styles.infoContainer}>
-        <View style={styles.rateContainer}>
-          <CustomIcon name="star" style={styles.starIcon} />
-          <Text style={styles.runtimeText}>
-            {movieData?.vote_average.toFixed(1)} ({movieData?.vote_count})
-          </Text>
-          <Text style={styles.runtimeText}>
-            {movieData?.release_date.substring(8, 10)}{' '}
-            {new Date(movieData?.release_date).toLocaleString('default', {
-              month: 'long',
-            })}{' '}
-            {movieData?.release_date.substring(0, 4)}
-          </Text>
-        </View>
-        <Text style={styles.descriptionText}>{movieData?.overview}</Text>
-      </View>
-
-      <View>
-        <CategoryHeader title="Top Cast" />
-        <FlatList
-          data={movieCastData}
-          keyExtractor={(item: any) => item.id}
-          horizontal
-          contentContainerStyle={styles.containerGap24}
-          renderItem={({ item, index }) => (
-            <CastCard
-              shouldMarginatedAtEnd={true}
-              cardWidth={80}
-              isFirst={index == 0 ? true : false}
-              isLast={index == movieCastData?.length - 1 ? true : false}
-              imagePath={baseImagePath('w185', item.profile_path)}
-              title={item.original_name}
-              subtitle={item.character}
-            />
-          )}
-        />
+    <SafeAreaView style={styles.mainContent}>
+      <ScrollView style={styles.container} bounces={false} showsVerticalScrollIndicator={false}>
+        <StatusBar hidden />
+        <MovieDetailsHeader movieData={movieData} />
+        <MovieDetailsInfo movieData={movieData} />
+        <MovieRate movieData={movieData} />
 
         <View>
-          <TouchableOpacity
-            style={styles.buttonBG}
-            onPress={() => {
-              navigation.push('SeatBooking', {
-                BgImage: baseImagePath('w780', movieData.backdrop_path),
-                PosterImage: baseImagePath('original', movieData.poster_path),
-              })
-            }}
-          >
-            <Text style={styles.buttonText}>Select Seats</Text>
-          </TouchableOpacity>
+          <CategoryHeader title="Top Cast" />
+          <FlatList
+            data={movieCastData}
+            keyExtractor={(item: any) => item.id}
+            horizontal
+            contentContainerStyle={styles.containerGap24}
+            renderItem={({ item, index }) => (
+              <CastCard
+                shouldMarginatedAtEnd={true}
+                cardWidth={80}
+                isFirst={index == 0 ? true : false}
+                isLast={index == movieCastData?.length - 1 ? true : false}
+                imagePath={baseImagePath('w185', item.profile_path)}
+                title={item.original_name}
+                subtitle={item.character}
+              />
+            )}
+          />
+
+          <View>
+            <TouchableOpacity
+              style={styles.buttonBG}
+              onPress={() => {
+                navigation.push('SeatBooking', {
+                  BgImage: baseImagePath('w780', movieData.backdrop_path),
+                  PosterImage: baseImagePath('original', movieData.poster_path),
+                })
+              }}
+            >
+              <Text style={styles.buttonText}>Select Seats</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
