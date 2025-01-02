2df84e93561233034292169f72aa04eedb0a2209
Avtandil Prangishvili <avtofrangi@gmail.com>
2025-01-02 20:26:29 +0400

    chore: Update TicketScreen

--- src/screens/TicketScreen/TicketScreen.tsx
+++ src/screens/TicketScreen/TicketScreen.tsx
@@ -12,9 +12,10 @@ const TicketScreen = ({ navigation, route }: any) => {
   const [ticketData, setTicketData] = useState<any>(route.params)
 
   /**
-   * useEffect hook to run once on component mount.
+   * useEffect hook that runs once when the component mounts.
    * It attempts to fetch ticket data from encrypted storage.
-   * If data is found, it updates the ticketData state.
+   * If data is found, it updates the ticketData state with the parsed JSON.
+   * Logs an error message if data retrieval fails.
    */
   useEffect(() => {
     ;(async () => {
