61be0f3e27af72d298fe69c4bb324d9c2d0f38f0
avtandil prangishvili <avtofrangi@gmail.com>
2025-03-12 16:57:27 +0400

    chore: Update TicketScreen component

--- src/screens/TicketScreen/TicketScreen.tsx
+++ src/screens/TicketScreen/TicketScreen.tsx
@@ -13,7 +13,6 @@ const TicketScreen = ({ navigation, route }: any) => {
 
   /**
    * useEffect hook that runs on component mount to retrieve and process ticket data.
-   * Fetches the stored ticket information from EncryptedStorage and updates the
    */
   useEffect(() => {
     ;(async () => {
