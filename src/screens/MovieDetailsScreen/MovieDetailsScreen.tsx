d325a472378060297eb36e24ba08d10bd453c540
avtandil prangishvili <avtofrangi@gmail.com>
2025-03-15 18:13:29 +0400

    chore: Update MovieDetailsScreen

--- src/screens/MovieDetailsScreen/MovieDetailsScreen.tsx
+++ src/screens/MovieDetailsScreen/MovieDetailsScreen.tsx
@@ -21,8 +21,6 @@ const MovieDetailsScreen = ({ navigation, route }: any) => {
  /**
    * The searchMoviesFunction is an asynchronous function that fetches movie data
    * from the API based on the provided search query. It makes a network request
-   * using the searchMovies endpoint and updates the searchList state with the
-   * to the console.
    * @param name - The search query string to find matching movies
    */
   useEffect(() => {
