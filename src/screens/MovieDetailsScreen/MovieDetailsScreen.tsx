e6e8628af587ac0dd185dd62fce9a6593735846b
avtandil prangishvili <avtofrangi@gmail.com>
2025-03-18 17:23:06 +0400

    fix: fix bug

--- src/screens/MovieDetailsScreen/MovieDetailsScreen.tsx
+++ src/screens/MovieDetailsScreen/MovieDetailsScreen.tsx
@@ -20,7 +20,6 @@ const MovieDetailsScreen = ({ navigation, route }: any) => {
 
  /**
    * The searchMoviesFunction is an asynchronous function that fetches movie data
-   * @param name - The search query string to find matching movies
    */
   useEffect(() => {
     const fetchMovieData = async () => {
