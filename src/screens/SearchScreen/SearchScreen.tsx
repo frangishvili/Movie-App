41e01f2a23ebb2a5d9dfded01f8fef4a71426756
avtandil prangishvili <avtofrangi@gmail.com>
2025-05-28 17:54:58 +0400

    chore: Update comment

--- src/screens/SearchScreen/SearchScreen.tsx
+++ src/screens/SearchScreen/SearchScreen.tsx
@@ -13,7 +13,7 @@ const SearchScreen = ({ navigation }: any) => {
   const [searchList, setSearchList] = useState([])
 
   /**
- * Searches for movies by name using the
+ * Searches for movies by name using 
  */
   const searchMoviesFunction = async (name: string) => {
     try {
